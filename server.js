ab4bb01a6790ebcb749a4fc7ad239689c848643c
Jan Nowak <jannowak@example.com>
2018-08-31 21:58:54 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,17 @@
+const express= require('express'),
+	 path = require('path');
+
+const app =express();
+
+
+app.use(express.static('./dist/TEST2'));
+
+app.get('/*', (req,res)=>{
+    
+res.sendFile(path.join(__dirname,'/dist/TEST2/index.html'));
+
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+console.log('Server started');
+})
