0fa77d18a734a4d1e0d9c06fc85713b258bdb306
Jan Nowak <jannowak@example.com>
2018-10-06 22:30:37 +0200

    ref

--- src/app/portal/section/content/content_item/textimages/textimages.component.ts
+++ src/app/portal/section/content/content_item/textimages/textimages.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialogRef, MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-textimages',
@@ -6,6 +8,16 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./textimages.component.css']
 })
 export class TextimagesComponent {
+  constructor(public dialog: MatDialog) { }
+  
   @Input()
   data;
+  fileNameDialogRef: MatDialogRef<DialogComponent>;
+
+  openDialog(data) {
+console.log(data)
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: {data: data}
+  })
+}
 }
