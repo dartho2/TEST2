812af4a2e098a246e1356ffe52cbe7de83f8b4da
Jan Nowak <jannowak@example.com>
2018-10-10 01:41:23 +0200

    cal

--- src/app/portal/section/content/content_item/table/table.component.ts
+++ src/app/portal/section/content/content_item/table/table.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MatDialog, MatDialogRef} from '@angular/material';
-import {PortalComponent } from '../../../../portal.component';
-import {DialogComponent} from '../dialog/dialog.component';
-declare var jquery:any;
-declare var $ :any;
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { PortalComponent } from '../../../../portal.component';
+import { DialogComponent } from '../dialog/dialog.component';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-table',
@@ -15,37 +15,39 @@ export class TableComponent implements OnInit {
   data;
 
   fileNameDialogRef: MatDialogRef<DialogComponent>;
-  
+
   constructor(
     public dialog: MatDialog,
     public portals: PortalComponent) { }
-  
-    openDialog(nameClass) {
-      const dialogRef = this.dialog.open(DialogComponent, {
-        data: {data: nameClass, type_class: "text"}
+
+  openDialog(nameClass) {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: { data: nameClass, type_class: "text" }
     })
   }
 
   getDataType() {
-      if (!this.data) {
-        return 'none';
-      } else if (Array.isArray(this.data)){
-        return 'array'
-      } else if (this.data.content) {
-        return 'content'
-      } else {
-        return 'string'
-      } 
+    if (!this.data) {
+      return 'none';
+    } else if (Array.isArray(this.data)) {
+      return 'array'
+    } else if (this.data.content) {
+      return 'content'
+    } else {
+      return 'string'
+    }
   }
   ngOnInit() {
-    
+
 
     $(document).ready(function () {
       $('td.table-o-a:not(:first-child)').click(function () {
         $(this).closest('tr').find('td.table-o-a:not(:first-child)').removeClass('selected');
+        $(this).closest('tr').find('td.table-o-a:not(:first-child)').removeAttr('data-value', 1);
         $(this).addClass('selected');
+        $(this).attr('data-value_test', 1)
       });
     })
   }
-  
+
 }
