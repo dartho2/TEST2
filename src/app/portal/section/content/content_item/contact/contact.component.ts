622e798a2834a6537ce8d77ef26f619b369dacfb
Jan Nowak <jannowak@example.com>
2018-10-17 01:49:05 +0200

    contact

--- src/app/portal/section/content/content_item/contact/contact.component.ts
+++ src/app/portal/section/content/content_item/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, HostListener, OnInit} from '@angular/core';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
 import { ConnectionService } from './connection.service';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 declare var jquery: any;
@@ -9,7 +9,7 @@ declare var $: any;
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit{
+export class ContactComponent implements OnInit {
   @Input()
   data;
   sender = false;
@@ -18,50 +18,47 @@ export class ContactComponent implements OnInit{
 
   getErrorMessage() {
     return this.email.hasError('required') ? 'Wpisz email' :
-        this.email.hasError('email') ? 'Nie poprawny email' :
-            '';
+      this.email.hasError('email') ? 'Nie poprawny email' :
+        '';
   }
-  
+
   contactForm: FormGroup;
-disabledSubmitButton: boolean = false;
-optionsSelect: Array<any>;
+  disabledSubmitButton: boolean = false;
+  optionsSelect: Array<any>;
 
   @HostListener('input') oninput() {
 
-  if (this.contactForm.valid) {
-    this.disabledSubmitButton = false;
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false;
     }
   }
 
   constructor(private fb: FormBuilder, private connectionService: ConnectionService) {
 
-  this.contactForm = fb.group({
-    'name': ['', Validators.required],
-    'from': ['', Validators.compose([Validators.required, Validators.email])],
-    'message': ['', Validators.required]
+    this.contactForm = fb.group({
+      'name': ['', Validators.required],
+      'from': ['', Validators.compose([Validators.required, Validators.email])],
+      'message': ['', Validators.required]
     });
   }
 
   onSubmit() {
     this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
       this.sender = true;
-      window.setTimeout(function() {
-        $(".alert").fadeTo(500, 0).slideUp(500, function(){
-            $(this).remove(); 
+
+      window.setTimeout(function () {
+        $(".alert").fadeTo(500, 0).slideUp(500, function () {
+          $(this).remove();
         });
-    }, 4000);
+      }, 4000);
       this.contactForm.reset();
       this.disabledSubmitButton = true;
     }, error => {
       console.log('Error', error);
     });
   }
-  ngOnInit(){
-    window.setTimeout(function() {
-      $(".alert").fadeTo(500, 0).slideUp(500, function(){
-          $(this).remove(); 
-      });
-  }, 4000);
+  ngOnInit() {
+
   }
-  
+
 }
