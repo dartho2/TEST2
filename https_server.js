9438f1f8277dd6612185243400f126c395f5800f
Jan Nowak <jannowak@example.com>
2018-10-24 13:30:02 +0200

    serv

--- https_server.js
+++ https_server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('fs');
+const http = require('http');
 const https = require('https');
-
-
-const options = {
-  key: fs.readFileSync('./cert/2BABF81C34AB39706EA004FE027FF72F.key'),
-  cert: fs.readFileSync('./cert/2BABF81C34AB39706EA004FE027FF72F.crt')
+const fs = require('fs');
+const privateKey = fs.readFileSync('./cert/2BABF81C34AB39706EA004FE027FF72F.key');
+const certyficate = fs.readFileSync('./cert/2BABF81C34AB39706EA004FE027FF72F.crt');
+const credentials = {
+  key: privateKey,
+  cert: certyficate
 }
 
 app.use(express.static('./dist/TEST2'));
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/TEST2/index.html'));
 });
 
-app.listen(process.env.PORT || 80, () => {
-  console.log('Server started');
+httpServer.listen(80, () => {
+  console.log('Server Http started on 80')
 });
-https.createServer(options,app).listen(80);
+
+httpsServer.listen(443, () => {
+  console.log('Server Https started on 443')
+});
