0cebff0a999085bdc90abbcf4e182844aa583a32
Jan Nowak <jannowak@example.com>
2018-10-28 11:24:45 +0100

    pita

--- src/app/portal/section/content/content_item/table/table.component.ts
+++ src/app/portal/section/content/content_item/table/table.component.ts
@@ -67,9 +67,9 @@ export class TableComponent implements OnInit {
   
         $(result, thead).each(function () {
         $('table tr').last().html('<td data-label=""'+' class="result-test">Wynik</td>')
-        .append($('<td data-label="Vata"'+' class="result-test">').append(result[0]))
-        .append($('<td data-label="Pita"'+ ' class="result-test">').append(result[1]))
-        .append($('<td data-label="Kapha"'+ ' class="result-test">').append(result[2]))
+        .append($('<td data-label="Vata"'+' class="result-test">').append('',result[0]))
+        .append($('<td data-label="Pita"'+ ' class="result-test">').append('',result[1]))
+        .append($('<td data-label="Kapha"'+ ' class="result-test">').append('' ,result[2]))
         });
   
       }
