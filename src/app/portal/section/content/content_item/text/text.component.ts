bef2cac4a01eb4980aad039ee708effd755a2201
Dartho2 <entropiadarth@gmail.com>
2020-03-02 16:44:56 +0100

    Update text.component.ts
    a

--- src/app/portal/section/content/content_item/text/text.component.ts
+++ src/app/portal/section/content/content_item/text/text.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PortalComponent} from '../../../../portal.component';
 import {DomSanitizer} from '@angular/platform-browser';
-import { DynamicScriptLoaderService } from '../../../../../DynamicScriptLoaderService';
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
@@ -15,14 +14,9 @@ export class TextComponent implements OnInit  {
   constructor(public portal: PortalComponent, public renderer: DomSanitizer, private dynamicScriptLoader: DynamicScriptLoaderService) {}
   ngOnInit() {
     this.renderer.bypassSecurityTrustHtml(this.data.text);
-    this.loadScripts();
+    
   }
-   private loadScripts() {
-    // You can load multiple scripts by just providing the key as argument into load method of the service
-    this.dynamicScriptLoader.load('random-gen','random-num').then(data => {
-      // Script Loaded Successfully
-    }).catch(error => console.log(error));
-  }
+   
 
   
 }
